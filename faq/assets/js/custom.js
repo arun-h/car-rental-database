3ec7f8e02a1d2208b3e3307d5666cbb28b1548cf
Arun Hiremath <85949349+arun-h@users.noreply.github.com>
2024-03-23 07:56:27 +0530

    added database and JavaScript files.

---
+++ faq/assets/js/custom.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+function reveal() {
+    document.getElementById("hid").style.display = 'none';
+=======
+function reveal() {
+    document.getElementById("hid").style.display = 'none';
+>>>>>>> a2f9213d4c18992a03a68f4d1ef7dded9db250db
+}
---
+++ faq/assets/js/theme.js
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+=======
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+>>>>>>> a2f9213d4c18992a03a68f4d1ef7dded9db250db
+});
